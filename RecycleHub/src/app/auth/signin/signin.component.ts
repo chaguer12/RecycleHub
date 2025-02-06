f5abe5f04e9939afbe5121cb34978b64ba84ee64
zoolotoui <chag.med.amine@gmail.com>
2025-02-06 09:30:53 +0100

    RES-7 stat: finished log-in

--- RecycleHub/src/app/auth/signin/signin.component.ts
+++ RecycleHub/src/app/auth/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as bcrypt from 'bcryptjs';
 
 @Component({
   selector: 'app-signin',
@@ -7,5 +10,34 @@ import { Component } from '@angular/core';
   standalone:false
 })
 export class SigninComponent {
+  email: string = '';
+  password: string = '';
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder,private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 
-}
+  onLogin() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      const users = JSON.parse(localStorage.getItem('users') || '[]');
+
+      
+      const user = users.find((u: any) => u.email === email);
+
+      if (user && bcrypt.compareSync(password, user.password)) {
+        localStorage.setItem('isAuthenticated', 'true');
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        alert('Login successful!');
+        this.router.navigate(['/demand']); 
+      } else {
+        alert('Invalid email or password.');
+      }
+    }
+  }
+  }
+
+
